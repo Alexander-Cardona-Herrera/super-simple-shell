01316a144b852491183bfbb1803c10b80a83a99b
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-04-03 19:26:52 -0500

    archivo fwe

---
+++ forkwait.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+  
+int main()
+{
+    
+    char *argv[] = {"/tmp", "ls", "-l", NULL};
+
+    for(int i = 0; i < 5 ;i++)
+	{
+		if(fork() == 0)
+		{
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+            if (execve(argv[0], argv, NULL) == -1)
+                perror("Error");
+
+            exit(0);
+        }
+    }
+    
+	for(int i = 0; i < 5 ;i++)
+	wait(NULL);
+	
+    return (0);
+}
